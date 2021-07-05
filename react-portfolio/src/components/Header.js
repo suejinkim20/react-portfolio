82ca3eb8f0e739a38e111a26d60b1de21d1ee609
Suejin Kim <kim.suejin.20@gmail.com>
2021-07-04 23:27:04 -0400

    added css

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
-//import '../styles/Header.css';
+//import '../styles/Header.css';  
 
 function Header() {
   return (
     <header className="header">
-      <h1>Home</h1>
+        <article class="about-me-photo">
+            <img src="./images/suejinkim-photo.jpg" width="150px" />
+        </article>  
+            <h1>Sue Jin Kim</h1>
+            <p class="subheader">aspiring web developer</p>
+            <p>HTML, CSS, JavaScript, NodeJS, Express, MongoDB</p>
     </header>
   );
 }
